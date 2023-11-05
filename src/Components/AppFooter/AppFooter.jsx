31c87f3cf1f59443b5fd48cd97f450108472d996
iSRHcoder <s.r.hatkadke@gmail.com>
2023-11-05 11:37:37 +0530

    added github icon in footer

--- src/Components/AppFooter/AppFooter.jsx
+++ src/Components/AppFooter/AppFooter.jsx
@@ -1,9 +1,17 @@
 import styles from "./AppFooter.module.css";
+import { FaGithub } from "react-icons/fa";
 
 const AppFooter = () => {
   return (
     <>
-      <div className={styles.footer}>`Made with ❤️ by iSRHcoder [2023]`</div>
+      <div className={styles.footer}>
+        <div>
+          <a href="https://github.com/iSRHcoder" style={{ color: "black" }}>
+            <FaGithub />
+          </a>
+        </div>
+        <div>`Made with ❤️ by iSRHcoder [2023]`</div>
+      </div>
     </>
   );
 };
